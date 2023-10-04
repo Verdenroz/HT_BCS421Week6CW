87d86eddd1b4d03b3ad22c20a4cd013957ef4dcd
harvey <tsenh@farmingdale.edu>
2023-10-04 16:34:37 -0400

    Added seekbar to set text in EditText and change font size in TextView

--- app/src/main/java/edu/farmingdale/bcs421/bcs421_f22_w6_fragmentsdemo/MainActivity2.kt
+++ app/src/main/java/edu/farmingdale/bcs421/bcs421_f22_w6_fragmentsdemo/MainActivity2.kt
@@ -3,12 +3,16 @@ package edu.farmingdale.bcs421.bcs421_f22_w6_fragmentsdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.SeekBar
 import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
     lateinit var mBtn1:Button
     lateinit var mBtn2:Button
     lateinit var mTV:TextView
+    lateinit var mEditText:EditText
+    lateinit var mSeekBar:SeekBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +21,29 @@ class MainActivity2 : AppCompatActivity() {
         mBtn1 = findViewById(R.id.btn1);
         mBtn2 = findViewById(R.id.btn2);
         mTV = findViewById(R.id.tv1)
+        mEditText = findViewById(R.id.editText01)
+        mSeekBar = findViewById(R.id.seekBar)
         var frgmnt01 = Fragment01()
         var frgmnt02 = Fragment02()
+
+        mSeekBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seek: SeekBar,
+                progress: Int, fromUser: Boolean
+            ) {
+                mEditText.setText(progress.toString())
+                mTV.textSize = progress.toFloat()
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                mTV.textSize = p0?.progress?.toFloat()!!
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                mTV.textSize = p0?.progress?.toFloat()!!
+            }
+        })
 
 
         mBtn1.setOnClickListener{
